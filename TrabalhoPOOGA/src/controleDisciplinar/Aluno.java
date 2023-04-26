a504934564825bd264e2bbb4accb4f2ffa05527d
FeltenMilena <gs758665@gmail.com>
2023-04-26 18:17:23 -0300

    Adicionado os atributos a classe

--- TrabalhoPOOGA/src/controleDisciplinar/Aluno.java
+++ TrabalhoPOOGA/src/controleDisciplinar/Aluno.java
@@ -1,10 +1,54 @@
 package controleDisciplinar;
+import java.util.List;
 
 public class Aluno {
+    
+    private int matricula;
+    private int cpf;
+    private String nome;
+    private String dataNasc;
+    
+    public Aluno() {
+    	
+    }
 
-	public static void main(String[] args) {
-		System.out.print("Olá Mundo");
-		//teste
+    public Aluno(int matricula, int cpf, String nome, String dataNasc) {
+        this.matricula = matricula;
+        this.cpf = cpf;
+    	this.nome = nome;
+    	this.dataNasc = dataNasc;
+    }
+    
+    public int getMatricula() {
+		return matricula;
 	}
 
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(String dataNasc) {
+		this.dataNasc = dataNasc;
+	}
 }
+
