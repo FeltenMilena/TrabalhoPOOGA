1f53a0d60fa6096a466f06885cfd88727eaeda5c
Milena Felten <gs758665@gmail.com>
2023-04-26 16:24:34 -0300

    Update Aluno.java
    Teste

--- TrabalhoPOOGA/src/controleDisciplinar/Aluno.java
+++ TrabalhoPOOGA/src/controleDisciplinar/Aluno.java
@@ -3,7 +3,8 @@ package controleDisciplinar;
 public class Aluno {
 
 	public static void main(String[] args) {
-		System.out.print("Olá Mundo");
+	    System.out.print("Olá Mundo");
+		//nsincs
 	}
 
 }
